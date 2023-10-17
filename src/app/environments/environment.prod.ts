b0fa381a9e0d925ebf458d89f5fede2fc64b8f7d
apb8176 <56334384+apb8176@users.noreply.github.com>
2023-10-17 22:23:59 +0530

    login fix

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    redirectUri:'http://localhost:4200/',
+    redirectUri:'http://localhost:4200',
     //redirectUri:'http://app-backend-bj3z634zumnje.azurewebsites.net/landing/chat',
     clientId:'fa1b7ab9-10a1-4bcb-8c5d-0df14dfbde8e',  
     authority:'https://login.microsoftonline.com/62057686-3148-4f3e-af18-1fd76b7e331f',
